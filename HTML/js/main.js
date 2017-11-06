a18c98954fd377950c081c1bbab63cf5ac595ea7
John Romani <john@anthood.com>
2017-11-06 17:02:30 +0100

    fixes in JS when deleting history

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -5,6 +5,7 @@ var trash_btns;
 
 function update_history()
 {
+
 	var formatted_list;// = "<div>" +  search_history[0].txt +" - "+ search_history[0].tim +"</div>";
 	for (var i=0; i<search_history.length; i++)
 	{
@@ -14,33 +15,22 @@ function update_history()
 			formatted_list += stylelist_item(i);
 	}
 
-	document.getElementById("history").innerHTML = formatted_list;
+	document.getElementById("history").innerHTML = (search_history.length >0)? formatted_list:'';
+	 
 }
 
 
 function stylelist_item(i)
 {
-	var str =
-	"<div>" + search_history[i].txt +" - "+ search_history[i].tim +
-		 				"<button onclick='tster("+search_history[i].listid+")'>del</button></div>";
+	var str = "<div><a><span>"+search_history[i].txt+"</span><span>"+search_history[i].tim+"</span></a><a class='del-history' onclick='killer("+search_history[i].listid+")'><img src='img/search.svg'></a></div>"
 	return str;
 }
 
-function tster (n){
-	console.log(n);
+function killer (n){
 	search_history.splice(n, 1);
 	update_history();
-
 }
 
-   
-trash_btns = document.getElementsByClassName("del-history");
-
-trash_btns[0].onclick = function() {myFunction()};
-
-function myFunction() {
-    console.log("fwef");
-}
 
 
 now_time = timestamp_to_date(Date.now())
